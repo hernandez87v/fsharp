c5a191acf6fc17127dafff7e7865b8ab5d26f429
hernandez87v <hernandez87v@hotmail.com>
2021-08-17 15:38:25 -0700

    add value bindings and explicit annotation examples

--- values.fsx
+++ values.fsx
@@ -25,3 +25,15 @@ let cityName = "Vancouver"
 let fSharpIsAwesome = true
 
 let luckyNumber = 13
+
+let coordinates = (1.0, 3.0)
+
+let toDoList = ["Learn F#"; "Build app"; "Profit!"]
+
+let divideByThree input = input / 3
+
+(*--------- EXPLICIT TYPE ANNOTATIONS ---------*)
+
+let (luckyNumberString:string) = "13"
+
+(*--------- UPDATING VALUES ---------*)
