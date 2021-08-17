a5b4c21338cf9957e91ad8ce7773d37f92ebbdb4
hernandez87v <hernandez87v@hotmail.com>
2021-08-17 15:30:44 -0700

    first commit

---
+++ values.fsx
@@ -0,0 +1,27 @@
+(*--------- VALUE TYPES ---------*)
+
+// String Value
+"Vancouver"
+
+// Boolean Value
+true
+
+// Integer Value
+17
+
+// Tuple
+(1.0, 5.0)
+
+// List Value
+["Learn F#"; "Build App"; "Profit!"]
+
+// Lambda expression / Anonymous function value
+fun input -> input / 3
+
+(*--------- VALUE BINDING ---------*)
+
+let cityName = "Vancouver"
+
+let fSharpIsAwesome = true
+
+let luckyNumber = 13
